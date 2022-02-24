65b865b40d83da970c10c353e79246f4e9e4b627
plusn <91170882+plusn5@users.noreply.github.com>
2022-02-24 17:28:23 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   /// int a;
+   /// printf("使用者輸入一個值:");
+   /// scanf("%d",&a);
+
+   /// printf("你讀到了 %d\n",a);
+
+    char c;
+    printf("請輸入一堆字: ");
+    scanf("%c",&c);
+    printf("你讀到了%c\n",c);
+
+    return 0;
+}
+
